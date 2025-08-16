249e8faf3979fb2384f1a9895bc8feb993b5158e
Sally M <smei201900@gmail.com>
2025-08-16 02:19:37 -0400

    added some comments about next steps for self reference, will be deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 //import { useState } from 'react'
 import './App.css'
+
 function App() {
+    
 }
 
 export default App
+
+
+/*we need the weather for ONE day 
+it should show the temperature, description of the weather (ex: sunny, runny, overcast clouds, thunderstorm), 
+    an icon representing the weather 
+We need a search bar to find the weather of a place 
+the searchbar should be replaced by the name of the place 
+
+other weather information includes humidity/rain, sunset?, wind speed, ??? represented by the stonks icon
+
+sunset might be uv index also 
+
+*/
+
+/*
+https://www.flaticon.com/free-animated-icons/weather 
+https://basmilius.github.io/weather-icons/ 
+https://fontawesome.com/icons/categories/weather 
+*/
