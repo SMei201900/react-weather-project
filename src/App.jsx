92ab56397bcd7d1187cbb82987037397d1d748d5
Sally M <smei201900@gmail.com>
2025-08-18 19:29:23 -0400

    deleted some comments that were used as reference for what my next steps may be

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,3 @@ const App = () => {
 }
 
 export default App
-
-/*
-https://www.flaticon.com/free-animated-icons/weather 
-https://fontawesome.com/icons/categories/weather 
-*/
-
-/*MAYBES
-https://www.flaticon.com/free-animated-icon/hurricane_17858153 
-https://www.flaticon.com/free-animated-icon/flood_18821565 
-https://www.flaticon.com/free-animated-icon/twister_11688571 
-https://www.flaticon.com/free-animated-icon/drought_17858201 
-*/
