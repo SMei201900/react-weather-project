255cfe89a30cd3f03fa45ba6f51a9f8f750a51c2
Sally M <smei201900@gmail.com>
2025-08-17 23:39:54 -0400

    imported all the icon images over, currently not in use, just testing

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './Weather.css';
 import search_icon from '../assets/search-icon.jpg'
+import cloudy_icon from '../assets/cloudy-icon.png'
+import rain_icon from '../assets/rain-icon.png'
+import snowy_icon from '../assets/snowy-icon.png'
+import sun_icon from '../assets/sun-icon.png'
+import thunderstorm_icon from '../assets/thunderstorm-icon.png'
+import windy_icon from '../assets/windy-icon.png'
+import humidity_icon from '../assets/humidity-icon.png'
 
 const Weather = () => {
   return (
@@ -10,6 +17,9 @@ const Weather = () => {
             <input type="text" placeholder='Chicago' />
             <img src={search_icon} alt="magnifying glass image to represent a search icon" />
         </div>
+        <img src={cloudy_icon} alt="clouds" className='weatherIcon'/>
+        <p>16</p>
+        <p>London</p>
     </div>
   )
 }
