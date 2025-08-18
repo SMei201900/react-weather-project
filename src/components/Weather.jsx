7b1a4c467b8e3545185fc46b4cca3a6acee4c06e
Sally M <smei201900@gmail.com>
2025-08-18 01:31:22 -0400

    added placeholders for humidity and wind speed values; mostly did styling for these components though

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './Weather.css';
 import search_icon from '../assets/search-icon.jpg'
 import cloudy_icon from '../assets/cloudy-icon.png'
+import windy_icon from '../assets/wind-icon.png'
+import humidity_icon from '../assets/humidity-icon.png'
 
 /*
 import rain_icon from '../assets/rain-icon.png'
 import snowy_icon from '../assets/snow-icon.png'
 import sun_icon from '../assets/sun-icon.png'
-import thunderstorm_icon from '../assets/thunderstorm-icon.png'
-import windy_icon from '../assets/wind-icon.png'
-import humidity_icon from '../assets/humidity-icon.png'*/
+import thunderstorm_icon from '../assets/thunderstorm-icon.png'*/
+
 
 const Weather = () => {
   return (
@@ -22,6 +23,22 @@ const Weather = () => {
         <img src={cloudy_icon} alt="clouds" className='weatherIcon'/>
         <p className="temp">16 <a href="">&deg;C</a></p>
         <p className="location">London</p>
+        <div className="weatherData"> 
+          <div className="weatherData-col">          
+              <img src={humidity_icon} alt="Humidity represented by water droplets" className='weatherDataIcon'/>
+              <div className="weatherDataContents">
+                <span>Humidity: </span>
+                <p>91 % </p>
+              </div> 
+            </div>
+            <div className="weatherData-col">          
+              <img src={windy_icon} alt="A icon representing wind speed" className='weatherDataIcon'/>
+              <div>
+                <span>Wind Speed</span>
+                <p>3.5 km/h</p>
+              </div> 
+            </div>
+        </div>
     </div>
   )
 }
