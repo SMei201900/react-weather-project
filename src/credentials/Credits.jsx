9aca336e35e9bd782bfcdb06e7babd1bee1c81ed
Sally M <smei201900@gmail.com>
2025-08-18 21:52:18 -0400

    added credentials, still need styling

---
+++ src/credentials/Credits.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Credits = () => {
+  return (
+    <div className="credit">
+		Created by {""}
+        <a href="https://www.linkedin.com/in/sally-mei-1a4439210/"
+			  target="_blank"
+			  rel="noreferrer">
+        Sally Mei 
+        </a>
+	  </div>  
+  )
+}
+
+export default Credits
