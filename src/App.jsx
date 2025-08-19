94703b2ce7dc4d163784a9840cb5b40567034c3b
Sally M <smei201900@gmail.com>
2025-08-18 21:52:18 -0400

    added credentials, still need styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useRef} from 'react'
 import Weather from './components/WeatherApp'
 import './App.css';
+import Credits from './credentials/Credits';
 
 const App = () => {
   const userinput = useRef(); 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="app-container">
         <Weather userinput={userinput}/> {/*using props here*/}
+        <Credits />
     </div>
   )
 }
