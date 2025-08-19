457bd9639544fb0245b5e7d926a95c4f9c8f86dd
Sally M <smei201900@gmail.com>
2025-08-18 21:07:38 -0400

    added comments so that I understand what I did

--- src/components/ConvertTemp.jsx
+++ src/components/ConvertTemp.jsx
@@ -2,17 +2,20 @@ import {useState} from 'react';
 
 const ConvertTemp = ({ celsius }) => {
   const [isCelsius, setIsCelsius] = useState(true); 
-    /*we are given degC at the beginnging thus true*/
+    /*we are given degC at the beginning thus true*/
 
-  const toggleUnit = () => { /*we want to*/
-    setIsCelsius(!isCelsius);
+  const toggleUnit = () => {
+    setIsCelsius(!isCelsius); /*we want to change Celsius to NOT Celsius */
   };
 
   const fahrenheit = (celsius * 9) / 5 + 32;
 
+
+  /*so when we click on the link we call toggleUnit that makes setIsCel to false now 
+    which allows us to then call fahrenheit but rounded*/
   return (
     <a onClick={toggleUnit}>
-      {isCelsius ? `${celsius}°C` : `${fahrenheit.toFixed(1)}°F`}
+    {isCelsius ? `${celsius}°C` : `${fahrenheit.toFixed(1)}°F`}
     </a>
   );
 };
