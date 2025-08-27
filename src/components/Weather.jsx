64c80802185bf5899c0d125256293b8a640ed87e
Sally M <smei201900@gmail.com>
2025-08-27 19:43:14 -0400

    fixing an exposure

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -28,15 +28,3 @@ const Weather = () => {
 
 export default Weather
 
-
-/*we need the weather for ONE day 
-it should show the temperature, description of the weather (ex: sunny, runny, overcast clouds, thunderstorm), 
-    an icon representing the weather 
-We need a search bar to find the weather of a place 
-the searchbar should be replaced by the name of the place 
-
-other weather information includes humidity/rain, sunset?, wind speed, ??? represented by the stonks icon
-
-sunset might be uv index also 
-
-*/
